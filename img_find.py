2861d15d1c61a7d86fa55f82173d5a3875c2a82a
ubkin <hivy777@gmail.com>
2020-07-19 21:08:58 +0300

    firs working script

--- img_find.py
+++ img_find.py
@@ -5,9 +5,8 @@ url = 'https://www.linux.org.ru/gallery/archive'
 domen = 'https://www.linux.org.ru'
 
 regex = r'/gallery/archive/\d+/\d+'
-regex_img = r'https://www.linux.org.ru/images/\d+/original/\d+'
+regex_img = r'https://www.linux.org.ru/images/\d+/original.jpg'
 
-#all mouth in gallery
 names = []
 
 def get_refs(url):
@@ -22,28 +21,9 @@ for date in get_refs(url):
     tmp = domen + str(date)
     names.append(tmp)
 
-sort = search(names, regex)
+names = search(names, regex)
 
-total = []
-
-for i in sort:
-    total.append(get_refs(i))
-
-img = search(total, regex_img)
-
-for i in img:
-    print(i)
-
-
-#refs = BeautifulSoup(response, 'lxml').find_all('a', href=True)
-
-#get all mouths
-#for date in [ref for ref in [x.get('href') for x in refs] if re.search(regex, str(ref))]:
-#    tmp = domen + str(date)
-#    names.append(tmp)
-#
-#for i in names:
-#    response = requests.get(i).text
-#    refs = BeautifulSoup(response, 'lxml').find_all('a', href=True)
-#    for image in [ref for ref in [x.get('href') for x in refs] if re.search(regex_img, str(ref))]:
-#        print(image)
+for i in names:
+    for n in get_refs(i):
+        if re.search(regex_img, n):
+            print(n)
