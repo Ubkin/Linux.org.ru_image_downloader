5b9659d5fc4faa9739bdad6a16fc6c119deed2c5
ubkin <hivy777@gmail.com>
2020-07-19 17:06:44 +0300

    script update

--- img_find.py
+++ img_find.py
@@ -1,6 +1,8 @@
 import re, requests
-from bs4 import BeautifulSoup4 as bs
+from bs4 import BeautifulSoup as bs
 
 url = 'https://www.linux.org.ru/gallery/archive'
 domen = 'https://www.linux.org.ru'
 expression = r'/gallery/archive/\d+/\d+'
+
+print(bs(requests.get(url).text, 'lxml'))
